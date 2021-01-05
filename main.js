69a899b887dc4c79531263567e92658c9498ca57
jodongha <66076070+jodongha@users.noreply.github.com>
2021-01-05 22:07:35 +0900

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-let getNowTime = () => {
-    let timeInMs = new Date();
-    let nowTime = timeInMs.getHours() + ":" + timeInMs.getMinutes();
-    let time = document.getElementById("time");
-    time.innerHTML = nowTime;
-}
-
-getNowTime();
-setInterval(getNowTime, 60000);
